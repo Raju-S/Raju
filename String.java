50021b48e46f464c616a19218285a6bb0a056ff0
Raju-S <rasuraju80@gmail.com>
2017-06-05 15:09:20 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,10 @@
+public class String
+{
+  public static void main(String args[])
+   {
+     String no = "10";
+     int result = Integer.parseInt(no);
+     System.out.println("The conversion of string to integer is : "+result);
+
+   }
+}

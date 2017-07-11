19cf88b8b4bfbc1a8e0dc66aa9b194644cea8138
Raju-S <rasuraju80@gmail.com>
2017-07-11 11:11:52 +0530

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,33 +1,26 @@
-package guvi;
-
-import java.util.Scanner;
+package cse;
+import java.util.*;
 
 public class Array {
 
-	public static void main(String args[]){
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		int sum = 0;
-		int i = 0;
-		System.out.println("Enter the Size of the Array :");
+	
+		System.out.println("enterv the size:");
 		int n = scan.nextInt();
-		int a[] = new int[n];
-		int count[] = new int[10];
-		for(i=0;i<n;i++){
+		int [] a = new int[n];
+		for(int i=0;i<n;i++){
 			a[i] = scan.nextInt();
-			sum = sum+a[i];
 		}
-		System.out.println("Sum of the Array : "+sum);
-		for(i=0;i<a.length;i++){
-			
-			int temp = a[i];
-			while(a[i]>0){
-				count[a[i]%10]++;
-				a[i]/=10;
+		
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				if(a[i]+a[j]==0){
+					System.out.println(a[i]+" "+a[j]+ " => "+(a[i]+a[j]));
+				}
 			}
-			a[i]/=10;
-		}
-		for(i=0;i<10;i++){
-			System.out.println(i+" occur "+count[i]+" times");
 		}
 	}
+
 }
